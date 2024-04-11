81ee309155f223381a68ca225a9a78ac1e67523c
Star <125512934+StaryLOL@users.noreply.github.com>
2024-04-10 21:23:08 -0700

    Update Notis.lua

--- Notis.lua
+++ Notis.lua
@@ -1,1 +1,7 @@
 
+return {
+    "Don't drink and derive!",
+    "Why don't scientists trust atoms? Because they make up everything!",
+    "I told my wife she was drawing her eyebrows too high. She looked surprised.",
+    "Why don’t skeletons fight each other? They don’t have the guts."
+}
