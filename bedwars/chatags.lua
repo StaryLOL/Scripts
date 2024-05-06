4abc37ef353bfe49d4449881a1db52dcee8c90e6
Star <125512934+StaryLOL@users.noreply.github.com>
2024-05-05 21:20:42 -0700

    Update chatags.lua

--- bedwars/chatags.lua
+++ bedwars/chatags.lua
@@ -1,5 +1,7 @@
 return {
     [1] = {name = "STAR", color = Color3.fromRGB(178, 0, 178)},
     [2] = {name = "TEST", color = Color3.fromRGB(255, 76.5, 76.5)},
+    [3] = {name = "TEST", color = Color3.fromRGB(255, 50, 50)},
 }
+
 
